a001694bf7248a7c63a4b30568fc950cf76e11e3
Marcin Grzejszczak <mgrzejszczak@3432.local>
2017-05-30 14:06:51 +0200

    Added atom packages

--- install_mac.sh
+++ install_mac.sh
@@ -12,10 +12,13 @@ sudo chown -R `whoami`:admin /usr/local/bin
 sudo chown -R `whoami`:admin /usr/local/opt
 
 # Install default applications
-brew install wget midnight-commander Caskroom/cask/mcedit vim Caskroom/cask/vagrant ansible maven mr bash"
+brew install wget midnight-commander Caskroom/cask/mcedit vim Caskroom/cask/vagrant ansible maven mr bash
 
 # git kurwa
 cp .gitconfig ~/
+
+# install zshrc
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 ## INSTALL ALL THE JDKS ANS STUFF
 
@@ -46,7 +49,7 @@ wget -O /tmp/atom-mac.zip https://github.com/atom/atom/releases/download/v1.2.4/
 unzip /tmp/atom-mac.zip -d /Applications
 
 # INSTALL ATOM PACKAGES
-apm install language-groovy an-color-picker atom-beautify merge-conflicts minimap file-icons travis-ci-status open-recent monokai-seti seti-ui todo-show highlight-selected minimap-highlight-selected autoclose-html pigments linter linter-javac linter-shellcheck linter-jsonlint linter-js-yaml auto-detect-indentation atom-beautify rest-client
+apm install language-groovy atom-beautify merge-conflicts minimap file-icons travis-ci-status open-recent monokai-seti seti-ui todo-show highlight-selected minimap-highlight-selected autoclose-html pigments linter linter-javac linter-shellcheck linter-jsonlint linter-js-yaml auto-detect-indentation atom-beautify rest-client atom-maven intellij-idea-keymap
 
 # DOCKER
 # http://mageinferno.com/blog/using-docker-machine-os-x-dinghy
