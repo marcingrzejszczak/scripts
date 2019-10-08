d7d010c1ee394f3948d32599ffd8a416c661f464
Marcin Grzejszczak <marcin.grzejszczak@gmail.com>
2019-10-07 22:41:22 -0500

    Updated

--- install_main.sh
+++ install_main.sh
@@ -22,7 +22,7 @@ cp .gitconfig ~/
 # gpg2 --list-keys
 # git config --global user.signingkey <KEY_NUMBER>
 # gpg2 --armor --export <KEY_NUMBER>
-git config --global commit.gpgsign true
+# git config --global commit.gpgsign true
 
 wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
 chsh -s `which zsh`
@@ -91,8 +91,8 @@ echo "deb https://apt.dockerproject.org/repo ubuntu-wily main" | sudo tee --appe
 sudo apt-get update -y
 sudo apt-get purge lxc-docker
 sudo apt-cache policy docker-engine
-sudo apt-get install linux-image-extra-$(uname -r)
-sudo apt-get install docker-engine
+sudo apt-get install linux-image-extra-$(uname -r) -y
+sudo apt-get install docker-engine -y
 sudo service docker start
 
 sudo curl -sSL https://get.docker.com/gpg | sudo apt-key add -
@@ -108,11 +108,11 @@ sudo chmod +x /usr/local/bin/docker-compose
 # https://www.torproject.org/projects/torbrowser.html.en
 
 # INSTALL ATOM
-wget -O /tmp/atom-amd64.deb https://github.com/atom/atom/releases/download/v1.1.0/atom-amd64.deb
-sudo dpkg --install /tmp/atom-amd64.deb
+# wget -O /tmp/atom-amd64.deb https://github.com/atom/atom/releases/download/v1.1.0/atom-amd64.deb
+# sudo dpkg --install /tmp/atom-amd64.deb
 
 # INSTALL ATOM PACKAGES
-apm install language-groovy atom-beautify merge-conflicts minimap file-icons travis-ci-status open-recent monokai-seti seti-ui todo-show highlight-selected minimap-highlight-selected autoclose-html pigments linter linter-javac linter-shellcheck linter-jsonlint linter-js-yaml auto-detect-indentation atom-beautify rest-client atom-maven intellij-idea-keymap
+# apm install language-groovy atom-beautify merge-conflicts minimap file-icons travis-ci-status open-recent monokai-seti seti-ui todo-show highlight-selected minimap-highlight-selected autoclose-html pigments linter linter-javac linter-shellcheck linter-jsonlint linter-js-yaml auto-detect-indentation atom-beautify rest-client atom-maven intellij-idea-keymap
 
 # Install ZOOM
 wget -O /tmp/zoom_amd64.deb https://zoom.us/client/latest/zoom_amd64.deb
@@ -130,12 +130,12 @@ sudo apt-get install -y build-essential
 #sudo pipelight-plugin --update
 
 # Install Slack client
-wget -O /tmp/slack.deb https://downloads.slack-edge.com/linux_releases/slack-desktop-2.3.4-amd64.deb
+wget -O /tmp/slack.deb https://downloads.slack-edge.com/linux_releases/slack-desktop-4.0.2-amd64.deb
 sudo dpkg -i /tmp/slack.deb
 sudo apt-get -f install -y
 
 # Install Jetbrains Toolbox
-wget -O /tmp/toolbox.tar.gz https://download.jetbrains.com/toolbox/jetbrains-toolbox-1.13.4733.tar.gz
+wget -O /tmp/toolbox.tar.gz https://download.jetbrains.com/toolbox/jetbrains-toolbox-1.15.5786.tar.gz
 mkdir $HOME/apps/JetBrains --parents
 tar -xf /tmp/toolbox.tar.gz -C $HOME/apps/JetBrains/
 ln -s $HOME/apps/JetBrains/jetbrains-toolbox-* $HOME/apps/JetBrains/toolbox
@@ -181,13 +181,18 @@ gem install bundler
 #bundle install
 
 # http://askubuntu.com/questions/626078/mouse-cursor-invisible-after-15-04-update
-sudo apt-get install gdm -y
+# sudo apt-get install gdm -y
 
 # INSTALL https://www.privateinternetaccess.com/pages/downloads
 echo "Private internet access installation"
-mkdir --parents ~/PIA
-curl https://www.privateinternetaccess.com/openvpn/openvpn.zip -o ~/PIA/openvpn.zip
-unzip ~/PIA/openvpn.zip -d ~/PIA
+#mkdir --parents ~/PIA
+#curl https://www.privateinternetaccess.com/openvpn/openvpn.zip -o ~/PIA/openvpn.zip
+#unzip ~/PIA/openvpn.zip -d ~/PIA
+cd ~
+wget https://installers.privateinternetaccess.com/download/pia-v82-installer-linux.tar.gz
+tar -xzf ./pia-v82-installer-linux.tar.gz
+./pia-v82-installer-linux.sh
+
 
 # INSTALL Spotify
 # 1. Add the Spotify repository signing keys to be able to verify downloaded packages
